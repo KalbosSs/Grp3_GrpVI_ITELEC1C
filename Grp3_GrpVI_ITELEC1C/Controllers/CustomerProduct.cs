c955e9102bdaf0b06c80a24e71a04a6e2779ba6c
Ivan Castro <cipcastro123@LAPTOP-44LB9K64>
2023-12-02 17:11:52 +0800

    Pogi ako

---
+++ Grp3_GrpVI_ITELEC1C/Controllers/CustomerProduct.cs
@@ -0,0 +1,31 @@
+﻿using Grp3_GrpVI_ITELEC1C.Services;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Grp3_GrpVI_ITELEC1C.Controllers
+{
+    public class CustomerProduct : Controller
+    {
+
+        private IProductDataService _service;
+
+        public CustomerProduct(IProductDataService service)
+        {
+            _service = service;
+            
+        }
+        public async Task<IActionResult> Index()
+        {
+            var products = await _service.GetProductsAsync();
+            return View(products);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Order(int id)
+        {
+            var products = await _service.GetProductsAsync();
+            var product = products.Where(x => x.Id == id).FirstOrDefault();
+            return View(product);
+        }
+    }
+}
